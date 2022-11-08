272b0fca0996cf33d60072a406cc2729cd8ec74f
Lauri Saarela <lauri.saarela@gofore.com>
2022-11-08 11:20:25 +0200

    Added failsafe mechanism, userID empty checks in User Operations

--- VirtualFinland.UserAPI/src/VirtualFinland.UsersAPI/Activities/User/Operations/CreateSearchProfile.cs
+++ VirtualFinland.UserAPI/src/VirtualFinland.UsersAPI/Activities/User/Operations/CreateSearchProfile.cs
@@ -1,3 +1,4 @@
+using FluentValidation;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Swashbuckle.AspNetCore.Annotations;
@@ -29,6 +30,14 @@ public static class CreateSearchProfile
         public void SetAuth(Guid? userDbId)
         {
             this.UserId = userDbId;
+        }
+    }
+    
+    public class CommandValidator : AbstractValidator<Command>
+    {
+        public CommandValidator()
+        {
+            RuleFor(command => command.UserId).NotNull().NotEmpty();
         }
     }
 
