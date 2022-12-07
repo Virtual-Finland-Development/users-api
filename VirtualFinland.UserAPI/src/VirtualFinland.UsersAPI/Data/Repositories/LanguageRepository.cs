d6a62d47ea022cb2a2cdb80097d20bbdd550dfe5
Lauri Saarela <lauri.saarela@gofore.com>
2022-12-07 13:21:28 +0200

    LanguageRepositoru - languages filtered

--- VirtualFinland.UserAPI/src/VirtualFinland.UsersAPI/Data/Repositories/LanguageRepository.cs
+++ VirtualFinland.UserAPI/src/VirtualFinland.UsersAPI/Data/Repositories/LanguageRepository.cs
@@ -9,7 +9,7 @@ public class LanguageRepository : ILanguageRepository
     public Task<List<Language>> GetAllLanguages()
     {
         return Task.FromResult(CultureInfo.GetCultures(CultureTypes.AllCultures)
-            .Where(o => !string.IsNullOrEmpty(o.Name))
+            .Where(o => !string.IsNullOrEmpty(o.Name) && o.Name == o.TwoLetterISOLanguageName)
             .Select(o => new Language(o.Name,
                 o.DisplayName,
                 o.EnglishName,
