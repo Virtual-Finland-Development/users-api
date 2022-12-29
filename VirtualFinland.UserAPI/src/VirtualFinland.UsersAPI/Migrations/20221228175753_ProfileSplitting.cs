83b869557cda74e0ccaef53f8dafef1bfa9d1e97
lassi.patanen <lassi.patanen@gofore.com>
2022-12-29 08:52:54 +0200

    Created new db migration

---
+++ VirtualFinland.UserAPI/src/VirtualFinland.UsersAPI/Migrations/20221228175753_ProfileSplitting.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VirtualFinland.UserAPI.Migrations
+{
+    public partial class ProfileSplitting : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "ExternalIdentities",
+                keyColumn: "Id",
+                keyValue: new Guid("9ed28e82-6cb9-4c48-a27c-603c5a97221f"));
+
+            migrationBuilder.CreateTable(
+                name: "Certifications",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "character varying(512)", maxLength: 512, nullable: true),
+                    Type = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: true),
+                    Created = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Modified = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Certifications", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Educations",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    EducationLevelEnum = table.Column<int>(type: "integer", nullable: true),
+                    EducationField = table.Column<string>(type: "character varying(4)", maxLength: 4, nullable: true),
+                    GraduationDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    EducationOrganization = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: true),
+                    Created = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Modified = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Educations", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Languages",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    EscoUri = table.Column<string>(type: "text", nullable: true),
+                    LanguageCode = table.Column<string>(type: "character varying(3)", maxLength: 3, nullable: true),
+                    SkillLevelEnum = table.Column<int>(type: "integer", nullable: true),
+                    Created = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Modified = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Languages", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Occupations",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    NaceCode = table.Column<string>(type: "character varying(7)", maxLength: 7, nullable: true),
+                    EscoUri = table.Column<string>(type: "text", nullable: true),
+                    EscoCode = table.Column<string>(type: "character varying(5)", maxLength: 5, nullable: true),
+                    WorkMonths = table.Column<int>(type: "integer", nullable: true),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: true),
+                    Created = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Modified = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Occupations", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Occupations_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Permits",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: true),
+                    Type = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: true),
+                    Created = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Modified = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Permits", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Skills",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    EscoUrl = table.Column<string>(type: "text", nullable: true),
+                    LanguageCode = table.Column<string>(type: "character varying(3)", maxLength: 3, nullable: true),
+                    SkillLevelEnum = table.Column<int>(type: "integer", nullable: true),
+                    Created = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Modified = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Skills", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WorkPreferences",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    PreferredRegionEnum = table.Column<string>(type: "text", nullable: true),
+                    PreferredMunicipalityEnum = table.Column<string>(type: "text", nullable: true),
+                    EmploymentTypeCode = table.Column<string>(type: "text", nullable: true),
+                    WorkingTimeEnum = table.Column<int>(type: "integer", nullable: true),
+                    WorkingLanguageEnum = table.Column<string>(type: "text", nullable: true),
+                    Created = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Modified = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WorkPreferences", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_WorkPreferences_Users_Id",
+                        column: x => x.Id,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "ExternalIdentities",
+                columns: new[] { "Id", "Created", "IdentityId", "Issuer", "Modified", "UserId" },
+                values: new object[] { new Guid("07621d0e-085a-414e-a2a2-2302ce7e0f8b"), new DateTime(2022, 12, 28, 17, 57, 53, 791, DateTimeKind.Utc).AddTicks(5240), "a7c0db2e-a03f-4cf6-a5f2-290415bcb7fa", "7a2de561-7f7e-4e3c-bc02-68b261f6bd99", new DateTime(2022, 12, 28, 17, 57, 53, 791, DateTimeKind.Utc).AddTicks(5240), new Guid("5a8af4b4-8cb4-44ac-8291-010614601719") });
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("5a8af4b4-8cb4-44ac-8291-010614601719"),
+                columns: new[] { "Created", "Gender", "Modified" },
+                values: new object[] { new DateTime(2022, 12, 28, 17, 57, 53, 790, DateTimeKind.Utc).AddTicks(7550), "Other", new DateTime(2022, 12, 28, 17, 57, 53, 790, DateTimeKind.Utc).AddTicks(7550) });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Occupations_UserId",
+                table: "Occupations",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Certifications");
+
+            migrationBuilder.DropTable(
+                name: "Educations");
+
+            migrationBuilder.DropTable(
+                name: "Languages");
+
+            migrationBuilder.DropTable(
+                name: "Occupations");
+
+            migrationBuilder.DropTable(
+                name: "Permits");
+
+            migrationBuilder.DropTable(
+                name: "Skills");
+
+            migrationBuilder.DropTable(
+                name: "WorkPreferences");
+
+            migrationBuilder.DeleteData(
+                table: "ExternalIdentities",
+                keyColumn: "Id",
+                keyValue: new Guid("07621d0e-085a-414e-a2a2-2302ce7e0f8b"));
+
+            migrationBuilder.InsertData(
+                table: "ExternalIdentities",
+                columns: new[] { "Id", "Created", "IdentityId", "Issuer", "Modified", "UserId" },
+                values: new object[] { new Guid("9ed28e82-6cb9-4c48-a27c-603c5a97221f"), new DateTime(2022, 11, 23, 7, 15, 9, 238, DateTimeKind.Utc).AddTicks(4460), "a8f51b88-af6b-4a95-b2d8-da62e0fb4f65", "15b205f4-98bc-491f-8616-ce66043ee7f6", new DateTime(2022, 11, 23, 7, 15, 9, 238, DateTimeKind.Utc).AddTicks(4460), new Guid("5a8af4b4-8cb4-44ac-8291-010614601719") });
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("5a8af4b4-8cb4-44ac-8291-010614601719"),
+                columns: new[] { "Created", "Gender", "Modified" },
+                values: new object[] { new DateTime(2022, 11, 23, 7, 15, 9, 238, DateTimeKind.Utc).AddTicks(4010), "0", new DateTime(2022, 11, 23, 7, 15, 9, 238, DateTimeKind.Utc).AddTicks(4010) });
+        }
+    }
+}
