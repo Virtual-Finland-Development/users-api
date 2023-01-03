e429f605c4e0994010e5a32c9e4b6e993b92fdeb
lassi.patanen <lassi.patanen@gofore.com>
2023-01-03 17:49:11 +0200

    Check eventData for null reference instead of entries

--- VirtualFinland.UserAPI/src/VirtualFinland.UsersAPI/Helpers/AuditInterceptor.cs
+++ VirtualFinland.UserAPI/src/VirtualFinland.UsersAPI/Helpers/AuditInterceptor.cs
@@ -6,42 +6,32 @@ namespace VirtualFinland.UserAPI.Helpers;
 
 public class AuditInterceptor : SaveChangesInterceptor
 {
-    public override ValueTask<InterceptionResult<int>> SavingChangesAsync(DbContextEventData eventData, InterceptionResult<int> result,
-        CancellationToken cancellationToken = new CancellationToken())
+    public override ValueTask<InterceptionResult<int>> SavingChangesAsync(
+        DbContextEventData eventData,
+        InterceptionResult<int> result,
+        CancellationToken cancellationToken = new())
     {
-        var insertedEntries = eventData.Context?.ChangeTracker
+        if (eventData.Context is null)
+            throw new ArgumentNullException(nameof(eventData), $"{nameof(eventData)} is null");
+
+        var insertedEntries = eventData.Context.ChangeTracker
             .Entries()
             .Where(x => x.State == EntityState.Added)
             .Select(x => x.Entity);
-        
-        if(insertedEntries is not null)
-        {
-            foreach (var insertedEntry in insertedEntries)
-            {
-                if (insertedEntry is Auditable auditableEntity)
-                {
-                    auditableEntity.Created = DateTime.UtcNow;
-                }
-            }
-        }
+
+        foreach (var insertedEntry in insertedEntries)
+            if (insertedEntry is Auditable auditableEntity)
+                auditableEntity.Created = DateTime.UtcNow;
 
         var modifiedEntries = eventData.Context?.ChangeTracker
             .Entries()
             .Where(x => x.State == EntityState.Modified)
             .Select(x => x.Entity);
 
-        if (modifiedEntries is not null)
-        {
-            foreach (var modifiedEntry in modifiedEntries)
-            {
-                if (modifiedEntry is Auditable auditableEntity)
-                {
-                    auditableEntity.Modified = DateTime.UtcNow;
-                }
-            }
-        }
-        
+        foreach (var modifiedEntry in modifiedEntries)
+            if (modifiedEntry is Auditable auditableEntity)
+                auditableEntity.Modified = DateTime.UtcNow;
+
         return base.SavingChangesAsync(eventData, result, cancellationToken);
-        
     }
 }
