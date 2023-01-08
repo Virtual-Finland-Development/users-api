c352ea1c27b76b38933b99dfde725bddd49059fb
lassi.patanen <lassi.patanen@gofore.com>
2023-01-08 15:13:45 +0200

    Updated test db seeding to use builder and added test for occupation delete operation

--- VirtualFinland.UsersAPI.UnitTests/Helpers/APIUserFactory.cs
+++ VirtualFinland.UsersAPI.UnitTests/Helpers/APIUserFactory.cs
@@ -1,37 +1,48 @@
 using Bogus;
 using VirtualFinland.UserAPI.Data;
-using VirtualFinland.UserAPI.Models.Shared;
 using VirtualFinland.UserAPI.Models.UsersDatabase;
 
 namespace VirtualFinland.UsersAPI.UnitTests.Helpers;
 
 public class APIUserFactory
 {
+    /// <summary>
+    ///     Create user with random guid
+    /// </summary>
+    /// <param name="dbContext"></param>
+    /// <returns></returns>
     public static async Task<(User user, ExternalIdentity externalIdentity)> CreateAndGetLogInUser(
         UsersDbContext dbContext)
     {
-        var faker = new Faker();
+        var dbUser = dbContext.Users.Add(new UserBuilder().Build());
 
-        var dbUser = dbContext.Users.Add(new User
+        var faker = new Faker();
+        var externalIdentity = dbContext.ExternalIdentities.Add(new ExternalIdentity
         {
-            StreetAddress = faker.Address.StreetAddress(),
-            ZipCode = faker.Address.ZipCode(),
-            City = faker.Address.City(),
-            Country = faker.Address.Country(),
             Created = DateTime.UtcNow,
             Modified = DateTime.UtcNow,
-            FirstName = faker.Person.FirstName,
-            LastName = faker.Person.LastName,
-            JobsDataConsent = true,
-            ImmigrationDataConsent = false,
-            CitizenshipCode = "FR",
-            CountryOfBirthCode = "FR",
-            OccupationCode = "4012",
-            NativeLanguageCode = "FR",
-            Gender = Gender.Male,
-            DateOfBirth = DateOnly.FromDateTime(DateTime.Now)
+            Issuer = faker.Random.String(10),
+            IdentityId = faker.Random.Guid().ToString(),
+            UserId = dbUser.Entity.Id
         });
 
+        await dbContext.SaveChangesAsync();
+
+        return (dbUser.Entity, externalIdentity.Entity);
+    }
+
+    /// <summary>
+    ///     Create user with specified guid as Id
+    /// </summary>
+    /// <param name="dbContext"></param>
+    /// <param name="userId"></param>
+    /// <returns></returns>
+    public static async Task<(User user, ExternalIdentity externalIdentity)> CreateAndGetLogInUser(
+        UsersDbContext dbContext, Guid userId)
+    {
+        var dbUser = dbContext.Users.Add(new UserBuilder().WithId(userId).Build());
+
+        var faker = new Faker();
         var externalIdentity = dbContext.ExternalIdentities.Add(new ExternalIdentity
         {
             Created = DateTime.UtcNow,
