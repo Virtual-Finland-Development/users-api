5d6ddb5378daa9c9a9b48720f9ec07aaaaa10038
lassi.patanen <lassi.patanen@gofore.com>
2023-01-17 09:47:48 +0200

    Remove unnecessary JsonPropertyName attributes

--- VirtualFinland.UserAPI/src/VirtualFinland.UsersAPI/Models/Shared/Gender.cs
+++ VirtualFinland.UserAPI/src/VirtualFinland.UsersAPI/Models/Shared/Gender.cs
@@ -5,7 +5,7 @@ namespace VirtualFinland.UserAPI.Models.Shared;
 [JsonConverter(typeof(JsonStringEnumConverter))]
 public enum Gender
 {
-    [JsonPropertyName("Other")] Other = 0,
-    [JsonPropertyName("Male")] Male = 1,
-    [JsonPropertyName("Female")] Female = 2
+    Other = 0,
+    Male = 1,
+    Female = 2
 }
