3f85f8acac87e2860236cb97aed6d3c538413fb5
lassi.patanen <lassi.patanen@gofore.com>
2023-01-18 09:03:59 +0200

    Updated regions to use same codes as before on atof demo

--- VirtualFinland.UserAPI/src/VirtualFinland.UsersAPI/Models/Shared/Region.cs
+++ VirtualFinland.UserAPI/src/VirtualFinland.UsersAPI/Models/Shared/Region.cs
@@ -5,29 +5,28 @@ using VirtualFinland.UserAPI.Helpers;
 namespace VirtualFinland.UserAPI.Models.Shared;
 
 /// <summary>
-/// Region code based on ISO 3166-2:FI standard
-/// https://fi.wikipedia.org/wiki/ISO_3166-2:FI
+///     Region code based on https://koodistot.suomi.fi/codescheme;registryCode=jhs;schemeCode=maakunta_1_20220101
 /// </summary>
 [JsonConverter(typeof(JsonEnumMemberValueConverterFactory))]
 public enum Region
 {
-    [EnumMember(Value = "01")] Ahvenanmaa = 1,
-    [EnumMember(Value = "02")] EtelaKarjala = 2,
-    [EnumMember(Value = "03")] EtelaPohjanmaa = 3,
-    [EnumMember(Value = "04")] EtelaSavo = 4,
-    [EnumMember(Value = "05")] Kainuu = 5,
-    [EnumMember(Value = "06")] KantaHame = 6,
-    [EnumMember(Value = "07")] KeskiPohjanmaa = 7,
-    [EnumMember(Value = "08")] KeskiSuomi = 8,
-    [EnumMember(Value = "09")] Kymenlaakso = 9,
-    [EnumMember(Value = "10")] Lappi = 10,
-    [EnumMember(Value = "11")] Pirkanmaa = 11,
-    [EnumMember(Value = "12")] Pohjanmaa = 12,
-    [EnumMember(Value = "13")] PohjoisKarjala = 13,
-    [EnumMember(Value = "14")] PohjoisPohjanmaa = 14,
-    [EnumMember(Value = "15")] PohjoisSavo = 15,
-    [EnumMember(Value = "16")] PaijatHame = 16,
-    [EnumMember(Value = "17")] Satakunta = 17,
-    [EnumMember(Value = "18")] Uusimaa = 18,
-    [EnumMember(Value = "19")] VarsinaisSuomi = 19
+    [EnumMember(Value = "21")] Ahvenanmaa = 21,
+    [EnumMember(Value = "09")] EtelaKarjala = 9,
+    [EnumMember(Value = "14")] EtelaPohjanmaa = 14,
+    [EnumMember(Value = "10")] EtelaSavo = 10,
+    [EnumMember(Value = "18")] Kainuu = 18,
+    [EnumMember(Value = "05")] KantaHame = 5,
+    [EnumMember(Value = "16")] KeskiPohjanmaa = 16,
+    [EnumMember(Value = "13")] KeskiSuomi = 13,
+    [EnumMember(Value = "08")] Kymenlaakso = 8,
+    [EnumMember(Value = "19")] Lappi = 19,
+    [EnumMember(Value = "06")] Pirkanmaa = 6,
+    [EnumMember(Value = "15")] Pohjanmaa = 15,
+    [EnumMember(Value = "12")] PohjoisKarjala = 12,
+    [EnumMember(Value = "17")] PohjoisPohjanmaa = 17,
+    [EnumMember(Value = "11")] PohjoisSavo = 11,
+    [EnumMember(Value = "07")] PaijatHame = 7,
+    [EnumMember(Value = "04")] Satakunta = 4,
+    [EnumMember(Value = "01")] Uusimaa = 1,
+    [EnumMember(Value = "02")] VarsinaisSuomi = 2
 }
