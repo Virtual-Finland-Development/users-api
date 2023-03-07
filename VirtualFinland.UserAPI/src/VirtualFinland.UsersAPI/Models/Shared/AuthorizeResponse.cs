1d13e7d0b1f4d3e440dbcba8bc26d74554929b6e
Lassi Piironen <lsipii@kapsi.fi>
2023-03-07 14:00:43 +0200

    Chore: authorize response json fields

--- VirtualFinland.UserAPI/src/VirtualFinland.UsersAPI/Models/Shared/AuthorizeResponse.cs
+++ VirtualFinland.UserAPI/src/VirtualFinland.UsersAPI/Models/Shared/AuthorizeResponse.cs
@@ -1,9 +1,14 @@
 
+using System.Text.Json.Serialization;
+
 namespace VirtualFinland.UserAPI.Models.Shared;
 
 public class AuthorizeResponse
 {
+    [JsonPropertyName("message")]
     public string Message { get; set; } = string.Empty;
+    [JsonPropertyName("authorization")]
     public AuthorizeResponseAuthorization Authorization { get; set; } = new AuthorizeResponseAuthorization();
+    [JsonPropertyName("consent")]
     public AuthorizeResponseConsent? Consent { get; set; }
 }
--- VirtualFinland.UserAPI/src/VirtualFinland.UsersAPI/Models/Shared/AuthorizeResponseAuthorization.cs
+++ VirtualFinland.UserAPI/src/VirtualFinland.UsersAPI/Models/Shared/AuthorizeResponseAuthorization.cs
@@ -1,11 +1,18 @@
 
+using System.Text.Json.Serialization;
+
 namespace VirtualFinland.UserAPI.Models.Shared;
 
 public class AuthorizeResponseAuthorization
 {
+    [JsonPropertyName("userId")]
     public string UserId { get; set; } = string.Empty;
-    public string? Email { get; set; }
+    [JsonPropertyName("email")]
+    public string Email { get; set; } = string.Empty;
+    [JsonPropertyName("expiresAt")]
     public int ExpiresAt { get; set; }
+    [JsonPropertyName("issuedAt")]
     public int IssuedAt { get; set; }
+    [JsonPropertyName("issuer")]
     public string Issuer { get; set; } = string.Empty;
 }
