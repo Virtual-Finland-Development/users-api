0523bfe1201b6ae39920be7635a20ef83035de34
Lassi Piironen <lsipii@kapsi.fi>
2023-03-11 15:28:58 +0200

    fix: readd the auth test

--- VirtualFinland.UsersAPI.UnitTests/Tests/Security/AuthenticationTests.cs
+++ VirtualFinland.UsersAPI.UnitTests/Tests/Security/AuthenticationTests.cs
@@ -2,6 +2,7 @@ using FluentAssertions;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Moq;
+using Moq.Protected;
 using VirtualFinland.UserAPI.Exceptions;
 using VirtualFinland.UserAPI.Helpers.Services;
 using VirtualFinland.UsersAPI.UnitTests.Helpers;
@@ -25,6 +26,34 @@ public class AuthenticationTests : APITestBase
 
         var authenticationService = new AuthenticationService(userSecurityService);
 
+        // Act
+        var act = () => authenticationService.GetCurrentUserId(mockHttpRequest.Object);
+
+        // Assert
+        await act.Should().ThrowAsync<NotAuthorizedException>();
+    }
+
+    [Fact]
+    public async Task Should_FailAuthGwVerificationIfEmptyToken()
+    {
+        // Arrange
+        await APIUserFactory.CreateAndGetLogInUser(_dbContext);
+        var mockLogger = new Mock<ILogger<UserSecurityService>>();
+        var mockHttpRequest = new Mock<HttpRequest>();
+        var mockHttpClientFactory = new Mock<IHttpClientFactory>();
+        var mockHeaders = new Mock<IHeaderDictionary>();
+        var mockHttpMessageHandler = new Mock<HttpMessageHandler>();
+        mockHttpMessageHandler.Protected()
+            .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+            .ReturnsAsync(new HttpResponseMessage { });
+        var httpClient = new HttpClient(mockHttpMessageHandler.Object);
+
+        mockHeaders.Setup(o => o.Authorization).Returns("");
+        mockHttpClientFactory.Setup(o => o.CreateClient(It.IsAny<string>())).Returns(httpClient);
+        mockHttpRequest.Setup(o => o.Headers).Returns(mockHeaders.Object);
+
+        var userSecurityService = new UserSecurityService(_dbContext, mockLogger.Object);
+        var authenticationService = new AuthenticationService(userSecurityService);
         // Act
         var act = () => authenticationService.GetCurrentUserId(mockHttpRequest.Object);
 
