c941924980444b766459509a935bbdc5920d9e6a
Lassi Piironen <lsipii@kapsi.fi>
2023-03-13 11:07:24 +0200

    refactor: providers config retry functionality use Task.Delay instead of Thread.Sleep

--- VirtualFinland.UserAPI/src/VirtualFinland.UsersAPI/Helpers/Configurations/TestBedConsentProviderConfig.cs
+++ VirtualFinland.UserAPI/src/VirtualFinland.UsersAPI/Helpers/Configurations/TestBedConsentProviderConfig.cs
@@ -7,37 +7,29 @@ namespace VirtualFinland.UserAPI.Helpers.Configurations;
 public class TestBedConsentProviderConfig : IConsentProviderConfig
 {
     private readonly string _jwksJsonUrl;
-    private readonly string _issuer;
-    private readonly string _consentVerifyUrl;
 
     private List<JsonWebKey> _keys = new List<JsonWebKey>();
 
-    private readonly int _configUrlMaxRetryCount = 5;
+    private const int _configUrlMaxRetryCount = 5;
     /// <summary>
     /// How long to wait in milliseconds before trying again to retrieve the openid configs
     /// </summary>
-    private readonly int _configUrlRetryWaitTime = 3000;
+    private const int _configUrlRetryWaitTime = 3000;
 
-    public string Issuer
-    {
-        get { return _issuer; }
-    }
+    public string Issuer { get; }
 
     public JsonWebKey? GetKey(string kid)
     {
         return _keys.FirstOrDefault(k => k.Kid == kid);
     }
 
-    public string ConsentVerifyUrl
-    {
-        get { return _consentVerifyUrl; }
-    }
+    public string ConsentVerifyUrl { get; }
 
     public TestBedConsentProviderConfig(IConfiguration configuration)
     {
         _jwksJsonUrl = configuration["Testbed:ConsentJwksJsonUrl"];
-        _issuer = configuration["Testbed:ConsentIssuer"];
-        _consentVerifyUrl = configuration["Testbed:ConsentVerifyUrl"];
+        Issuer = configuration["Testbed:ConsentIssuer"];
+        ConsentVerifyUrl = configuration["Testbed:ConsentVerifyUrl"];
     }
 
     public async void LoadPublicKeys()
@@ -52,7 +44,7 @@ public class TestBedConsentProviderConfig : IConsentProviderConfig
                 var jsonData = JsonSerializer.Deserialize<JwksJsonResponse>(await httpResponse.Content.ReadAsStringAsync());
                 if (jsonData == null || jsonData.Keys == null)
                 {
-                    throw new ApplicationException("Failed to retrieve TestBed Consent public key configurations");
+                    throw new Exception("Failed to retrieve TestBed Consent public key configurations");
                 }
 
                 _keys = jsonData.Keys.Select(k => new JsonWebKey()
@@ -64,17 +56,17 @@ public class TestBedConsentProviderConfig : IConsentProviderConfig
                     E = k.E
                 }).ToList();
             }
-            Thread.Sleep(_configUrlRetryWaitTime);
+            await Task.Delay(_configUrlRetryWaitTime);
         }
     }
 
-    class JwksJsonResponse
+    private class JwksJsonResponse
     {
         [JsonPropertyName("keys")]
         public List<Jwk>? Keys { get; set; }
     }
 
-    class Jwk
+    private class Jwk
     {
         [JsonPropertyName("kty")]
         public string? Kty { get; set; }
