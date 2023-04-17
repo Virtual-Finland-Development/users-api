8e4c0a5c040bb31e2b32bee50da36bc24866ab58
Lauri Saarela <lauri.saarela@gofore.com>
2023-04-17 09:38:43 +0300

    profile (user) deletion route/functionality added

---
+++ VirtualFinland.UserAPI/src/VirtualFinland.UsersAPI/Activities/User/Operations/DeleteUser.cs
@@ -0,0 +1,73 @@
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Swashbuckle.AspNetCore.Annotations;
+using VirtualFinland.UserAPI.Data;
+using VirtualFinland.UserAPI.Exceptions;
+using VirtualFinland.UserAPI.Helpers.Swagger;
+
+namespace VirtualFinland.UserAPI.Activities.User.Operations;
+
+public static class DeleteUser
+{
+    public class Command : IRequest<DeleteUserResponse>
+    {
+        public Command()
+        { }
+
+        [SwaggerIgnore]
+        public Guid? UserId { get; set; }
+
+        public void SetAuth(Guid? userDatabaseId)
+        {
+            UserId = userDatabaseId;
+        }
+    }
+
+    public class Handler : IRequestHandler<Command, DeleteUserResponse>
+    {
+        private readonly UsersDbContext _context;
+
+        public Handler(UsersDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<DeleteUserResponse> Handle(Command request,
+            CancellationToken cancellationToken)
+        {
+            var person = await _context.Persons
+                .Include(p => p.Occupations)
+                .Include(p => p.Educations)
+                .Include(p => p.LanguageSkills)
+                .Include(p => p.Skills)
+                .Include(p => p.Certifications)
+                .Include(p => p.Permits)
+                .Include(p => p.WorkPreferences)
+                .SingleAsync(p => p.Id == request.UserId, cancellationToken);
+            var externalIdentity = await _context.ExternalIdentities.SingleOrDefaultAsync(id => id.UserId == request.UserId);
+
+            try
+            {
+                _context.Persons.Remove(person);
+
+                if (externalIdentity != null)
+                {
+                    _context.ExternalIdentities.Remove(externalIdentity);
+                }
+
+                await _context.SaveChangesAsync(cancellationToken);
+            }
+            catch (DbUpdateException e)
+            {
+                throw new BadRequestException(e.InnerException?.Message ?? e.Message);
+            }
+
+
+            return new DeleteUserResponse
+            ();
+        }
+    }
+
+    [SwaggerSchema(Title = "DeleteUserResponse")]
+    public record DeleteUserResponse();
+}
