ac470ba8af7d89ebfefe5d7461721bd321f92767
Lassi Piironen <lsipii@kapsi.fi>
2023-05-12 14:01:52 +0300

    fix: collection comparator helpers caused warnings on possible null references

--- VirtualFinland.UserAPI/src/VirtualFinland.UsersAPI/Helpers/CollectionValueComparer.cs
+++ VirtualFinland.UserAPI/src/VirtualFinland.UsersAPI/Helpers/CollectionValueComparer.cs
@@ -5,8 +5,8 @@ namespace VirtualFinland.UserAPI.Helpers;
 public class CollectionValueComparer<T> : ValueComparer<ICollection<T>>
 {
     public CollectionValueComparer() : base(
-        (c1, c2) => c1.SequenceEqual(c2),
-        c => c.Aggregate(0, (a, v) => HashCode.Combine(a, v.GetHashCode())), 
+        (c1, c2) => Enumerable.SequenceEqual(c1 ?? Enumerable.Empty<T>(), c2 ?? Enumerable.Empty<T>()),
+        c => c.Aggregate(0, (a, v) => HashCode.Combine(a, v == null ? 0 : v.GetHashCode())),
         c => c.ToHashSet())
     {
     }
