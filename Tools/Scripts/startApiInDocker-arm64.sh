583e9f7da3ab4efbb76cf34edc85cc916241b818
Lassi Piironen <lsipii@kapsi.fi>
2023-06-05 18:56:20 +0300

    chore: cleanup the helper scripts

--- Tools/Scripts/startApiInDocker-arm64.sh
+++ Tools/Scripts/startApiInDocker-arm64.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
-
-echo "Starting Users API in docker"
-cd ./VirtualFinland.UserAPI/src/VirtualFinland.UsersAPI
-echo "Work Directory:"
-pwd
-echo "Building Docker Image of API"
 export USERAPI_DOCKERFILE=Dockerfile.arm64
-
-docker build -f ${USERAPI_DOCKERFILE} -t virtualfinland/usersapi:latest .
-echo "Starting LocalDev in Docker"
-cd ../../../
-echo "Work Directory:"
-pwd
-docker compose -f ./docker-compose.yml up
-docker compose -f ./docker-compose.yml down
+docker compose -f ./docker-compose.yml up --build
