6c45bbd6603344dedfaaae571dc5f2bc850a2d80
Lassi Piironen <lsipii@kapsi.fi>
2023-09-19 10:47:54 +0300

    fix: admin lambda function input parsing

--- VirtualFinland.UsersAPI.AdminFunction/AdminApp/Models/FunctionPayload.cs
+++ VirtualFinland.UsersAPI.AdminFunction/AdminApp/Models/FunctionPayload.cs
@@ -1,6 +1,9 @@
+using System.Text.Json.Serialization;
+
 namespace VirtualFinland.AdminFunction.AdminApp.Models;
 public record FunctionPayload
 {
+    [JsonConverter(typeof(JsonStringEnumConverter))]
     public Actions Action { get; init; }
     public string? Data { get; init; }
 }
--- VirtualFinland.UsersAPI.AdminFunction/Function.cs
+++ VirtualFinland.UsersAPI.AdminFunction/Function.cs
@@ -2,18 +2,15 @@ using Microsoft.Extensions.DependencyInjection;
 using VirtualFinland.UserAPI.Data;
 using VirtualFinland.AdminFunction.AdminApp;
 using VirtualFinland.AdminFunction.AdminApp.Models;
-using System.Text.Json;
-using VirtualFinland.AdminFunction.AdminApp.Actions;
+using Amazon.Lambda.Core;
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
 namespace VirtualFinland.AdminFunction;
 
 public class Function
 {
-    public static async Task FunctionHandler(string lambdaEvent)
+    public static async Task FunctionHandler(FunctionPayload payload)
     {
-        // Parse payload
-        var payload = JsonSerializer.Deserialize<FunctionPayload>(lambdaEvent) ?? throw new ArgumentNullException(nameof(lambdaEvent));
-
         // Setup
         using var app = await App.Build();
         using var scope = app.Services.CreateScope();
