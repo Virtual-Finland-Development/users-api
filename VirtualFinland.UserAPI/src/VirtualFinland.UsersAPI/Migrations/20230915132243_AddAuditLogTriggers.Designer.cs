0b6fcfe325678e61194a58cefc01523d6e7071c4
Lassi Piironen <lsipii@kapsi.fi>
2023-09-25 22:56:13 +0300

    fix: drop the previous ef version tryouts

--- VirtualFinland.UserAPI/src/VirtualFinland.UsersAPI/Migrations/20230915132243_AddAuditLogTriggers.Designer.cs
+++ VirtualFinland.UserAPI/src/VirtualFinland.UsersAPI/Migrations/20230915132243_AddAuditLogTriggers.Designer.cs
@@ -21,7 +21,7 @@ namespace VirtualFinland.UserAPI.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "7.0.5")
+                .HasAnnotation("ProductVersion", "6.0.7")
                 .HasAnnotation("Relational:MaxIdentifierLength", 63);
 
             NpgsqlModelBuilderExtensions.UseIdentityByDefaultColumns(modelBuilder);
