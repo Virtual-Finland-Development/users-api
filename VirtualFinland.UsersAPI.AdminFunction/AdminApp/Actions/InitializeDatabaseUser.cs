be7758f5447516ed33e6d9f5067cee35ebc51d39
Lassi Piironen <lsipii@kapsi.fi>
2023-09-29 13:45:27 +0300

    feat: as single transaction

--- VirtualFinland.UsersAPI.AdminFunction/AdminApp/Actions/InitializeDatabaseUser.cs
+++ VirtualFinland.UsersAPI.AdminFunction/AdminApp/Actions/InitializeDatabaseUser.cs
@@ -20,10 +20,18 @@ public class InitializeDatabaseUser : IAdminAppAction
             PropertyNameCaseInsensitive = true
         }) ?? throw new ArgumentException("Invalid credentials payload", nameof(credentialsPayload));
 
-        await dataContext.Database.ExecuteSqlRawAsync(
-            $"CREATE USER '{credentials.Username}' WITH PASSWORD '{credentials.Password}';");
-        await dataContext.Database.ExecuteSqlRawAsync(
-            $"GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA 'public' TO '{credentials.Username}';");
+        await dataContext.Database.ExecuteSqlRawAsync(@$"
+            -- Create role if not exists
+            DO $$ BEGIN IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'user') THEN CREATE ROLE user; END IF; END $$;
+            -- Grant permissions to the role
+            DO $$ BEGIN IF EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'user') THEN GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA 'public' TO user; END IF; END $$;
+            -- Remove all users from user role
+            DO $$ BEGIN FOR r IN (SELECT * FROM pg_user) LOOP IF EXISTS (SELECT 1 FROM pg_roles WHERE rolname = r.usename) THEN REVOKE user FROM r.usename; END IF; END LOOP; END $$;
+            -- Create user
+            CREATE USER '{credentials.Username}' WITH PASSWORD '{credentials.Password}';
+            -- Add user to user role
+            GRANT user TO '{credentials.Username}';
+        ");
     }
 
     public record DatabaseUserCredentials(string Username, string Password);
