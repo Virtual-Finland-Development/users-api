6d6b784929983966c2d91ad745a7f3f372887a31
Lassi Piironen <lsipii@kapsi.fi>
2023-10-13 16:17:23 +0300

    feat: add persistent data protection keys

--- VirtualFinland.UserAPI/src/VirtualFinland.UsersAPI/Data/UsersDBContext.cs
+++ VirtualFinland.UserAPI/src/VirtualFinland.UsersAPI/Data/UsersDBContext.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.DataProtection.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using VirtualFinland.UserAPI.Data.Configuration;
 using VirtualFinland.UserAPI.Helpers;
@@ -5,7 +6,7 @@ using VirtualFinland.UserAPI.Models.UsersDatabase;
 
 namespace VirtualFinland.UserAPI.Data;
 
-public class UsersDbContext : DbContext
+public class UsersDbContext : DbContext, IDataProtectionKeyContext
 {
     private readonly bool _isTesting;
     private readonly IAuditInterceptor _auditInterceptor;
@@ -32,6 +33,8 @@ public class UsersDbContext : DbContext
     public DbSet<PersonAdditionalInformation> PersonAdditionalInformation => Set<PersonAdditionalInformation>();
     public DbSet<Skills> Skills => Set<Skills>();
     public DbSet<WorkPreferences> WorkPreferences => Set<WorkPreferences>();
+    public DbSet<DataProtectionKey> DataProtectionKeys { get; set; } = null!;
+
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
