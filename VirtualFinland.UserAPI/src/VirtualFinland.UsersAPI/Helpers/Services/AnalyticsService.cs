6d3b8af45a3852e0c4cdb3b12536bb645d5bc89d
Lassi Piironen <lsipii@kapsi.fi>
2023-10-20 20:40:20 +0300

    style: neatify the event context name

--- VirtualFinland.UserAPI/src/VirtualFinland.UsersAPI/Helpers/Services/AnalyticsService.cs
+++ VirtualFinland.UserAPI/src/VirtualFinland.UsersAPI/Helpers/Services/AnalyticsService.cs
@@ -17,11 +17,11 @@ public class AnalyticsService<T> : ILogger<T>
     /// </summary>
     /// <param name="auditEvent">The audit log event</param>
     /// <param name="requestAuthenticatedUser">The authenticated user</param>
-    /// <param name="eventContextName">Context name for the log</param>
+    /// <param name="eventContextName">Context name for the log, defaults to the parsed form of the generic type T</param>
     /// <returns></returns>
     public void LogAuditLogEvent(AuditLogEvent auditEvent, RequestAuthenticatedUser requestAuthenticatedUser, string? eventContextName = null)
     {
-        if (eventContextName == null) eventContextName = _handlerType.ToString();
+        eventContextName ??= ParseHandlerTypeContextName();
 
         _logger.LogInformation("AuditLog: {auditEvent} ({eventContextName}) on {userInfo}",
             auditEvent.ToString().ToUpper(),
@@ -47,5 +47,22 @@ public class AnalyticsService<T> : ILogger<T>
     public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
     {
         _logger.Log(logLevel, eventId, state, exception, formatter);
+    }
+
+    /// <summary>
+    /// Parse the handler type context name from the handler type
+    /// eg. VirtualFinland.UserAPI.Activities.Productizer.Operations.BasicInformation.GetPersonBasicInformation+Handler -> GetPersonBasicInformation
+    /// </summary>
+    /// <returns></returns>
+    private string ParseHandlerTypeContextName()
+    {
+        var handlerType = _handlerType.ToString();
+        var handlerTypeParts = handlerType.Split('.');
+        var handlerTypeContextName = handlerTypeParts[^1];
+        if (handlerTypeContextName.Contains('+'))
+        {
+            handlerTypeContextName = handlerTypeContextName.Split('+')[0];
+        }
+        return handlerTypeContextName;
     }
 }
