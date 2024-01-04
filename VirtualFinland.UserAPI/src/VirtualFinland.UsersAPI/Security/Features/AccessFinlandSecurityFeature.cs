69fb30c41696d4126f84d20d8e3e4ddb21401925
Lassi Piironen <lsipii@kapsi.fi>
2024-01-04 14:01:21 +0200

    build: setup with pulumi, rename to virtual finland

--- VirtualFinland.UserAPI/src/VirtualFinland.UsersAPI/Security/Features/AccessFinlandSecurityFeature.cs
+++
@@ -1,8 +0,0 @@
-using VirtualFinland.UserAPI.Security.Models;
-
-namespace VirtualFinland.UserAPI.Security.Features;
-
-public class AccessFinlandSecurityFeature : SecurityFeature
-{
-    public AccessFinlandSecurityFeature(SecurityFeatureOptions configuration, SecurityClientProviders securityClientProviders) : base(configuration, securityClientProviders) { }
-}
